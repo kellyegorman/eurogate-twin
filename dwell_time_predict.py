e809f04b9a877b8e87a93ed02ce4c3bc769c8b56
RachelBlack003 <rblack003@drury.edu>
2026-06-08 16:56:59 +0200

    created shells for classes

--- dwell_time_predict.py
+++ dwell_time_predict.py
@@ -1,1 +1,17 @@
-print("hello world")
+class StraddleCarrier():
+    def __init__(self, id):
+        self.id = id
+
+class Container():
+    def __init__(self, id, size, weight, status, type, commodity):
+        self.id = id
+        self.size = size
+        self.weight = weight
+        self.status = status
+        self.type = type
+        self.commodity = commodity
+
+        #change this later. We probably need information
+        #like row, bay, and tier and maybe more.
+        self.location = None
+
